72ce088b18b1f79708aaf5655c1d6eda2e786dd6
Jair Andres <jair.quinonez@epn.edu.ec>
2021-06-22 11:52:42 -0500

    inicioClase11

--- MyTaller/app/src/main/java/com/example/mytaller/MainActivityClase10.kt
+++ MyTaller/app/src/main/java/com/example/mytaller/MainActivityClase10.kt
@@ -2,10 +2,13 @@ package com.example.mytaller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivityClase10 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_clase10)
+        //Logueador
+        Log.i("ciclo-vida","onCreate")
     }
 }
