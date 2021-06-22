a6a178833bae32aac1602a6f3ab03dfae6fac2f8
Jair Andres <jair.quinonez@epn.edu.ec>
2021-06-21 23:14:51 -0500

    Últimos cambios Android

---
+++ MyTaller/app/src/main/java/com/example/mytaller/MainActivityClase10.kt
@@ -0,0 +1,11 @@
+package com.example.mytaller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityClase10 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_clase10)
+    }
+}
