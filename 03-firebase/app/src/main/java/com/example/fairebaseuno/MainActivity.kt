0f437ecac4d2edf368d30136f6701f9271088c19
CinthiaBR <cinthia.burbano@epn.edu.ec>
2021-08-30 09:16:52 -0500

    Authentication firebase

--- 03-firebase/app/src/main/java/com/example/fairebaseuno/MainActivity.kt
+++ 03-firebase/app/src/main/java/com/example/fairebaseuno/MainActivity.kt
@@ -6,9 +6,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.TextView
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     val CODIGO_INICIO_SESION=102
@@ -44,16 +48,83 @@ class MainActivity : AppCompatActivity() {
             CODIGO_INICIO_SESION->{
                 if(resultCode== Activity.RESULT_OK){
                     val usuario= IdpResponse.fromResultIntent(data)
+                    //if(usuario!=null){
                     if(usuario?.isNewUser==true){
                         Log.i("firebase-login","Nuevo Usuario")
+                        registrarUsuarioPorPrimeraVez(usuario)
                     }else{
+                        setearUsuarioFirebase()
                         Log.i("firebase-login","Usuario Antiguo")
                     }
                 }else{
                     Log.i("firebase-login","El usuario Cancelo")
                 }
             }
+            }
         }
-    }
 
-}
+        fun setearUsuarioFirebase(){
+            val instanciaAuth = FirebaseAuth.getInstance()
+            val usuarioLocal = instanciaAuth.currentUser
+            if(usuarioLocal !=null){
+                if(usuarioLocal.email !=null){
+                    val db= Firebase.firestore
+                    val referencia = db.collection("usuario")
+                        .document(usuarioLocal.email.toString())
+                    referencia
+                        .get().addOnSuccessListener {
+                            val usuarioCargado: FirestoreUsuarioDto? = it.toObject(FirestoreUsuarioDto::class.java)
+                            if(usuarioCargado!=null){
+                                BAuthUsuario.usuario= BUsuarioFirebase(
+                                    usuarioCargado.uid,
+                                    usuarioCargado.email,
+                                    usuarioCargado.roles
+                                )
+                            }
+
+                            Log.i("firebase-firestore", "Usuario cargado")
+                        }
+                        .addOnFailureListener { Log.i("firebase-firestore", "fallo cargar usuario") }
+                }
+
+            }
+        }
+
+        fun setearBienvenida(){
+            val textViewBienvenida =findViewById<TextView>(R.id.tv_bienvenida)
+            if(BAuthUsuario.usuario != null){
+                textViewBienvenida.text ="Bienvenido ${BAuthUsuario.usuario?.email}"
+            }else{
+                textViewBienvenida.text= "Ingresa al aplicativo"
+            }
+        }
+        fun registrarUsuarioPorPrimeraVez(usuario:IdpResponse){
+                 val identificadorUsuario = usuario.email
+                val usuaruioLogeado= FirebaseAuth.getInstance()
+                    .getCurrentUser()
+                if(usuario.email !=null && usuaruioLogeado != null){
+                    //roles
+                    val db = Firebase.firestore
+                    val rolesUsuario= arrayListOf("usuario") //creamos el usuario de permisos
+                    val nuevoUsuario = hashMapOf<String, Any>(
+                        "roles" to rolesUsuario,
+                        "uid" to usuaruioLogeado.uid,
+                        "email" to identificadorUsuario.toString()
+                    )
+
+                    db.collection("usuario").document(identificadorUsuario.toString())//
+                        .set(nuevoUsuario)
+                        .addOnSuccessListener {
+                            Log.i("firebase-firestore", "Se creo")
+                            setearUsuarioFirebase()
+                        }
+                        .addOnFailureListener {
+                            Log.i("firebase-firestore", "Fallo")
+                        }
+
+
+                }else {
+                    Log.i("firebase-login","ERROR")
+                }
+            }
+        }
---
+++ Deber/app/src/main/java/com/example/myapplication_deber_02/detalleWish.kt
@@ -0,0 +1,4 @@
+package com.example.myapplication_deber_02
+
+data class detalleWish( val imagen :String, val descripcion:String, val image:String
+                   ,val precio2:String, val descripcion2:String, val image2:String)
